debd2286522064ae0a16a62f185f15f3a248b7b4
YAKNO666 <122855546+YAKNO666@users.noreply.github.com>
2023-02-16 18:07:58 +0900

    first commit

--- src/sorce/src_3cube/script.js
+++ src/sorce/src_3cube/script.js
@@ -1,10 +1,9 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { gsap } from 'gsap'
-import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/roundedboxgeometry'
-import * as dat from 'lil-gui'
-import { Vector3 } from 'three'
+import * as THREE from '../../node_modules/three'
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js'
+import { gsap } from '../../node_modules/gsap'
+import * as dat from '../../node_modules/lil-gui'
+import { Vector3 } from '../../node_modules/three'
 
 
 /**
